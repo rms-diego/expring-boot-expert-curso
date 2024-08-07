3136e9d5cc8b27b79ffd0a9054c8ca81c5210d2d
Diego Ramos <diegorbrito9@gmail.com>
2024-08-07 09:16:11 -0300

    secao 3: fim da aula 'configurando e obtendo conexões com banco de dados'

---
+++ secao3/src/main/java/com/vendas/secao3/Secao3Application.java
@@ -0,0 +1,13 @@
+package com.vendas.secao3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao3Application.class, args);
+	}
+
+}
---
+++ secao3/src/test/java/com/vendas/secao3/Secao3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas.secao3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
