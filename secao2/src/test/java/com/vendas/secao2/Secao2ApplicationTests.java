e6ad214f34af52d06f96ccd8668942579fdfb9e7
Diego Ramos <diegorbrito9@gmail.com>
2024-08-06 23:39:16 -0300

    secao 2: fim da aula 'hello world'

---
+++ secao2/src/test/java/com/vendas/secao2/Secao2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas.secao2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
